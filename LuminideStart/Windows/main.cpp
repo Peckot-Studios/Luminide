3faf3c13453c5bd730339b010da7cee1142ba132
Pectics <pectics@outlook.com>
2023-06-09 16:57:26 +0800

    构建

---
+++ LuminideStart/Windows/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    if (GetFileAttributesA("node_modules") == INVALID_FILE_ATTRIBUTES)
+    {
+        string choice = "y";
+        cout << "错误：未找到依赖！" << endl;
+        cout << "请确保已经安装 Node.js 和 npm 依赖！" << endl;
+        cout << "启动自动安装依赖？(Y/n)" << endl;
+        cin >> choice;
+        if (choice == "n")
+        {
+            exit(1);
+        }
+        else
+        {
+            cout << "正在安装…" << endl;
+            system("npm i");
+        }
+    }
+    else
+    {
+        cout << "检测到依赖文件!" << endl;
+        cout << "正在启动Luminide…" << endl;
+        system("node App.js");
+    }
+
+    return 0;
+}
